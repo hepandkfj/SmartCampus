b54331cee5e9a753d6b92668f50e5741b529ddde
HJS <714638848@qq.com>
2023-11-14 10:36:11 +0800

    初始化

---
+++ src/test/java/org/app/bootweb/BootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.app.bootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
